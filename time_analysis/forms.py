86f97ea9f9697d4fee662caddea7aa37402d47de
Vlad Shelemakha <vlad.shelemakha0302@gmail.com>
2022-10-25 10:45:24 +0300

    feat: add logging

--- time_analysis/forms.py
+++ time_analysis/forms.py
@@ -1,11 +1,10 @@
-from decimal import Decimal
+
 import numpy as np
 from pandas import DataFrame
 from utils.base_forms import AnalyticBaseForm 
 
 from utils.parser_files import convertor_file_to_df
-from django.forms import FileField, FloatField, IntegerField, ChoiceField, DecimalField
-from pandas import DataFrame
+from django.forms import FileField, FloatField, IntegerField, ChoiceField
 
 from utils.base_validators import validate_file_extension
 
